7ffecd00a0d4064a2165f6cd0fd9519b7d259eb3
henderiw <wim.henderickx@gmail.com>
2021-09-05 12:42:20 +0200

    updated boilerplate

--- internal/controllers/pkg/revision/reconciler.go
+++ internal/controllers/pkg/revision/reconciler.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 Wim Henderickx.
+Copyright 2021 NDD.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
